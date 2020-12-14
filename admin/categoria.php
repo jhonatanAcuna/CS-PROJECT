7f99760d9cc6a55abd6bc1de0709d26ccdf536e5
Jhonatan Acuñ <jacunitahuisa@gmail.com>
2020-12-14 18:54:46 -0500

    Edit Product Complete

--- admin/categoria.php
+++ admin/categoria.php
@@ -293,7 +293,7 @@
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
-	        <h5 class="modal-title" id="addProdModalLabel">Editar Menú</h5>
+	        <h5 class="modal-title" id="addProdModalLabel">Editar Producto</h5>
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	          <span aria-hidden="true">&times;</span>
 	        </button>
