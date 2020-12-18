9419ef7a772ef6eadc697601018ed31e9db51b2a
Jhonatan Acuñ <jacunitahuisa@gmail.com>
2020-12-17 21:05:04 -0500

    Index

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="es">
 <head>
-	<title>Restaurante</title>
-	<link rel="stylesheet" href="./style.css">
-
-
-<h1 style="text-align:center;">Restaurante</h1>
-
-
-	<a href="staff" >Empleado</a>
-	<a href="admin">Administrador</a>
-
-
-
-
-
-
+<title>Restaurant</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body>
+
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+
+    <div id="logo" class="fl_left">
+      <h1><a href="index.php">Restaurant</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a class="drop" href="#">Inicio de Sesión</a>
+          <ul>
+            <li><a href="admin">Administrador</a></li>
+            <li><a href="staff">Empleado</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </div>
-<!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
-
-
-	
+<div class="wrapper bgded overlay" style="background-image:url('image/back.jpg');">
+  <div id="pageintro" class="hoc clear"> 
+    
+    <article>
+    <p>Inicia Sesión para continuar</p>
+	  <h3 class="heading">Bienvenido Usuario </h3>
+    </article>
+    
+  </div>
+</div>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
 </body>
 </html>
